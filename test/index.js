771e6fa81657202e61a810db2cb7efc5758c0ce9
Xuyang Jia <jiaxuyang@outlook.com>
2019-07-19 16:15:19 +0800

    修改测试代码

--- test/index.js
+++ test/index.js
@@ -1,13 +1,4 @@
-const path = require('path')
-const fse = require('fs-extra')
-const { encryptFile, decryptFile } = require('../lib/cryptoUtils')
-const { algorithm, encryptKey, iv } = fse.readJSONSync('./encrypt_cfg.json')
+const { encryptSingleFile } = require('../src/assertsHandler')
 
-const source = 'icon.png'
-const encrypted = encryptFile(source, algorithm, encryptKey, iv)
-const encryptedName = `${path.basename(source, path.extname(source))}2${path.extname(source)}`
-fse.outputFileSync(encryptedName, encrypted)
-
-const decrypted = decryptFile(encryptedName, algorithm, encryptKey, iv)
-const decryptedName = `${path.basename(source, path.extname(source))}3${path.extname(source)}`
-fse.outputFileSync(decryptedName, decrypted)
+const source = './test/a.png'
+encryptSingleFile(source, './test/b.png')
